a6f7258c3cafc21bd1db9f41d7786eb20e7e9b9d
Ajaya Kumar <ajayakumar4741@gmail.com>
2026-02-06 22:18:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useContext, useEffect, useState } from 'react'
+
+import './App.css'
+import Navbar from './Components/Navbar'
+import { Outlet } from 'react-router-dom'
+import { UserContext } from './Components/UserContext'
+
+function App() {
+  const { user, setUser } = useContext(UserContext);
+  const [loading, setLoading] = useState(true); // New loading state
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser)); // Parse JSON string from localStorage
+    } else {
+      setUser(null);
+    }
+    setLoading(false); // Mark loading as complete
+  }, []);
+
+  if (loading) {
+    return <div className='spinner'></div>; // Optionally show a loading spinner
+  }
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </>
+  )
+}
+
+export default App
